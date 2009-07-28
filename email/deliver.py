f256bbb927fa53ea626665a053a36ec16326b800
Alice Bevan-McGregor <alice.mcgregor@me.com>
2009-07-28 13:55:00 -0700

    Added docstring to mail delivery script.

--- email/deliver.py
+++ email/deliver.py
@@ -1,3 +1,30 @@
+"""Bulk mailer.
+
+This takes a message from message.txt and mails it to the e-mail
+addresses listed, one per line, in members.txt.
+
+Make sure you have a complete e-mail, including headers.  I'd suggest
+adding (or using instead) the following headers for bulk traffic:
+
+Return-Path: <example+bounces@example.com>
+To: <example@example.com>
+Organization: Example Co.
+X-BeenThere: example@example.com
+Precedence: list
+Reply-To: example@example.com
+List-Id: Full Name for the Example List
+        <example.example.com>
+List-Unsubscribe: <http://example.com/unsubscribe>,
+        <mailto:example+unsubscribe@example.com>
+List-Archive: <http://example.com/archive>
+List-Help: <mailto:example+help@comoxvalleychamber.com>
+List-Subscribe: <http://example.com/subscribe>,
+        <mailto:example+subscribe@comoxvalleychamber.com>
+Sender: example+bounces@example.com
+Errors-To: example+bounces@example.com
+Subject: [Example List] Original Subject
+"""
+
 import logging, turbomail
 
 log = logging.getLogger('mlist')
