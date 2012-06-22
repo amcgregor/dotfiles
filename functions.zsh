9c9bc11d2f251e009d9b3d5d9d7d2fdcbc42240d
Alice Bevan-McGregor <alice@gothcandy.com>
2012-06-22 13:12:58 -0400

    Added automatic ls and virtualenv activation on cd.

--- functions.zsh
+++ functions.zsh
@@ -2,3 +2,12 @@
 # Dynamically loaded functions.
 
 fpath=($ZDOTDIR/func $fpath)
+
+# We do a few things every time the user changes directory.
+# First, run ls.  I do this so often manually that it's stupid.
+# Second, check to see if we just entered a virtual environment; if so, activate.
+function chpwd() {
+    emulate -L zsh
+    ls -a
+    [[ -e bin/activate ]] && source bin/activate
+}
