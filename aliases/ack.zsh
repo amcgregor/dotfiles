1e2862244ad114fd144981ff644a0c01714876c8
Alice Bevan-McGregor <alice@gothcandy.com>
2012-06-22 13:13:15 -0400

    Added useful ack shortcuts to search specific file types.

---
+++ aliases/ack.zsh
@@ -0,0 +1,9 @@
+# Alice's over-engineered z-shell configuration, released in the public domain.
+# Ack is nice.
+
+alias back='ack -C 3 --smart-case --sort-files'
+alias ahtml='back --type=html'
+alias acss='back --type=css'
+alias ajs='back --type=js'
+alias aless='back --type=less'
+alias apy='back --type=python'
