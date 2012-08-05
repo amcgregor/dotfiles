0d6b5c3e5e6a02523ba5dea73831df5b67863db4
Alice Bevan-McGregor <alice@gothcandy.com>
2012-08-04 20:04:30 -0400

    Who needs virtualenvwrapper?

--- functions.zsh
+++ functions.zsh
@@ -3,10 +3,20 @@
 
 fpath=($ZDOTDIR/func $fpath)
 
+# Great thanks to Valodim in #zsh for his assistance with this!
+parent() { [[ ${1[1,$#2]} == $2 ]] }
+sibling() { [[ ${1:a:h} == ${2:a:h} ]]  }
+
 # We do a few things every time the user changes directory.
-# First, run ls.  I do this so often manually that it's stupid.
-# Second, check to see if we just entered a virtual environment; if so, activate.
+# * If we are in a virtual environment, check to see if we have left it and deactivate.
+# * If we are still in a virtual environment, stop processing.
+# * Check to see if we just entered a virtual environment; if so, activate.
 function chpwd() {
     emulate -L zsh
+    
+    [[ -n $VIRTUAL_ENV ]] && [[ "$VIRTUAL_ENV" != "$PWD" ]] && { parent $VIRTUAL_ENV $PWD || sibling $VIRTUAL_ENV $PWD } && deactivate
+    [[ -n $VIRTUAL_ENV ]] && return
+    
     [[ -e bin/activate ]] && source bin/activate
+    [[ -e src ]] && cd src
 }
