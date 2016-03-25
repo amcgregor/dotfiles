fcc657b7ec3dd2608fce73476f3b427eeddc14f5
Alice Bevan–McGregor <alice@gothcandy.com>
2016-03-25 05:45:36 -0400

    Login information.

---
+++ .zsh/hook/zlogin/basic_information.zsh
@@ -0,0 +1,30 @@
+# Alice's over-engineered z-shell configuration.
+# Basic system information.
+
+[[ -o interactive ]] || return
+
+echo en "\n \033[1;32m*\033[0m "
+lsb_release -d -s | sed s/\"//g
+
+echo -en " \033[1;32m*\033[0m "
+uname -snmpr
+
+MAX=$(($(cat /proc/cpuinfo | grep processor | wc -l) - 1))
+LOAD=$(cat /proc/loadavg | cut -d '.' -f 1)
+
+if [ $LOAD -gt $(($MAX * 4)) ]
+then echo -en " \033[1;31m*\033[0m"
+elif [ $LOAD -gt $MAX ]
+then echo -en " \033[1;33m*\033[0m"
+else echo -en " \033[1;32m*\033[0m"
+fi
+uptime
+
+echo -en " \033[1;32m*\033[0m IPv4 Network Interfaces: \033[1m"
+ifconfig | grep 'inet ' | cut -d ' ' -f 10 | tr '\n' ' '
+echo -e "\033[0m"
+
+echo -en " \033[1;32m*\033[0m IPv6 Network Interfaces: \033[1m"
+ifconfig | grep 'inet6 ' | cut -d ' ' -f 10 | tr '\n' ' '
+echo -e "\033[0m"
+
---
+++ .zsh/hook/zlogin/dotfiles_uptodate.zsh
@@ -0,0 +1,42 @@
+# Alice's over-engineered z-shell configuratdion.
+# Verify if our dotfiles are up-to-date.
+
+# We only attempt this check for interactive sessions.
+
+[[ -o interactive ]] || return
+
+
+# Also skip the check if the home folder isn't dotfiles-managed.
+
+[[ -d ~/.dotfiles ]] || return
+
+GIT_DIR="${HOME}/.dotfiles"
+
+
+function ebegin () {
+	echo -en " \033[1;32m*\033[0m $1... "
+}
+
+function eend () {
+	local integer result=${1:=$?}
+	local message="${2:=failed}"
+	
+	[[ $result -eq 0 ]] && echo -e "\b\b\b\b: done." || echo -e "\b\b\b\b: ${message}."
+	
+	return 0
+}
+
+
+# Calculate if we have diverged from upstream.
+
+# TODO: Limit check to once per day.
+
+ebegin "Checking for dotfiles update"
+
+ours=`git rev-parse HEAD`
+theirs=`git ls-remote origin -h refs/heads/dotfiles`
+
+[[ "$ours" == "$theirs[(w)1]" ]] && eend || eend 1 "an update is availble"
+
+unset ours theirs
+
