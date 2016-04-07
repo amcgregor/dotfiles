e6a76d0f25fee3ed6fc348f2f89369812a4f6d08
Alice Bevan–McGregor <alice@gothcandy.com>
2016-04-07 13:08:17 -0400

    Armoured `clear` alias.

--- .zsh/hook/alias/shell.zsh
+++ .zsh/hook/alias/shell.zsh
@@ -1,16 +1,22 @@
 # Alice's over-engineered z-shell configuration, released in the public domain.
 # Shell builtin, file, and process management aliases.
 
+[[ -o interactive ]] || return
+
 # I like some of my commands to act consistently across platforms.
-if [[ "$VENDOR" == "apple" ]]; then
-    alias d='du -h -d 1'
-    alias grep='grep --colour'
-    alias clear="osascript -e 'tell application \"System Events\" to keystroke \"k\" using command down'"
-    alias ls='ls -G'
-    alias lock='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
+if [[ $OSTYPE == darwin* ]]; then
+	alias d='du -h -d 1'
+	alias grep='grep --colour'
+	alias ls='ls -G'
+	
+	# These are distinctly Mac.
+	alias lock='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
+	
+	# This should only be aliased if running locally (!ssh) and when not wrapped in screen/tmux.
+	[[ $TTY == /dev/tty([sS]|)<-> ]] && alias clear="osascript -e 'tell application \"System Events\" to keystroke \"k\" using command down'"
 else
-    alias d='du --total -h --max-depth=1'
-    alias ls='ls --color=auto'
+	alias d='du --total -h --max-depth=1'
+	alias ls='ls --color=auto'
 fi
 
 # This next line permits alias expansion of the first argument after each command.
