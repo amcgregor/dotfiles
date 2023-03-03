5c6113b37b0b8b1fd72fbf8db413748ddcb23f06
Alice Zoë Bevan–McGregor <alice@gothcandy.com>
2023-03-03 12:21:30 -0500

    Brew upgrade alias.

--- .zsh/hook/zlogin/homebrew.zsh
+++ .zsh/hook/zlogin/homebrew.zsh
@@ -2,4 +2,6 @@ if [[ -e /opt/homebrew ]]; then
 	export HOMEBREW_NO_ENV_HINTS=1
 	
 	eval "$(/opt/homebrew/bin/brew shellenv)"
+	
+	alias brewup='clear; brew update; clear; brew outdated; echo -en "Proceed? "; read; clear; brew upgrade'
 fi
